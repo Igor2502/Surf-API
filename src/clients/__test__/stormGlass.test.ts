6040239ba86a668285228d7d8eb0a35968672282
Igor <igor.gds.00@gmail.com>
2021-05-08 14:10:12 -0300

    Utilizando Jest Mocks com Typescript

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,15 +6,14 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormGlass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('shoul return the normalized forecas from the StormGlass service', async () => {
     const lat = 58.7984;
     const lng = 17.8081;
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -35,7 +35,12 @@ export class StormGlass {
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1620425897&lat=${lat}&lng=${lng}`
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1620425897&lat=${lat}&lng=${lng}`,
+      {
+        headers: {
+          Authorization: 'fake-token'
+        }
+      }
     );
 
     return this.normalizeResponse(response.data);
