63c2dd8ba607ddd2068d1f6db6a3bbd79edea6ad
Igor <igor.gds.00@gmail.com>
2021-05-08 19:37:29 -0300

    Adicionando configuração dinamica com node-config

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
 import { InternalError } from '@src/util/errors/internal-error';
 import { AxiosStatic } from 'axios';
+import config, { IConfig } from 'config';
 
 export interface StormGlassSource {
   [key: string]: number;
@@ -44,6 +45,7 @@ export class StormGlassResponseError extends InternalError {
   }
 }
 
+const stormGlassRessourceConfig: IConfig = config.get('App.resources.StormGlass');
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -53,10 +55,10 @@ export class StormGlass {
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
       const response = await this.request.get<StormGlassForecastResponse>(
-        `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1620425897&lat=${lat}&lng=${lng}`,
+        `${stormGlassRessourceConfig.get('apiUrl')}/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1620425897&lat=${lat}&lng=${lng}`,
         {
           headers: {
-            Authorization: 'fake-token'
+            Authorization: stormGlassRessourceConfig.get('apiToken')
           }
         }
       );
